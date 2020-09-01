139ebc27554aca3407877ff55ba576bf2fdb3c16
Pedro Duarte <contact@peduarte.com>
2020-09-01 11:08:07 +0200

    Update variant names

--- components/Container.tsx
+++ components/Container.tsx
@@ -12,19 +12,19 @@ export const Container = styled.div({
 
   variants: {
     size: {
-      $1: {
+      '1': {
         maxWidth: '300px',
       },
-      $2: {
+      '2': {
         maxWidth: '585px',
       },
-      $3: {
+      '3': {
         maxWidth: '865px',
       },
-      $4: {
+      '4': {
         maxWidth: '1145px',
       },
-      $5: {
+      '5': {
         maxWidth: 'none',
       },
     },
