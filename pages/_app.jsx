6b38ad4ef875fb5984fc86152742fc342b702c59
Vlad Moroz <hi@vladmoroz.com>
2020-10-21 14:59:41 +0300

    Safari fixes and slight style adjustments

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,7 +11,7 @@ function App({ Component, pageProps }) {
     <div className={theme}>
       <Head>
         <title>Design System</title>
-        <link rel="stylesheet" href="https://core.modulz.app/fonts/fonts.css" />
+        <link rel="stylesheet" href="https://develop.modulz.app/fonts/fonts.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
