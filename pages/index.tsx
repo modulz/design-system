4b070cc4e82f24eedb44c7967b0eac66737d701e
Colm Tuite <colmtuite@gmail.com>
2020-11-16 02:21:00 +0000

    Add VerifiedBadge component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import { Text } from '../components/Text';
 import { Link } from '../components/Link';
 import { Kbd } from '../components/Kbd';
 import { Separator } from '../components/Separator';
+import { VerifiedBadge } from '../components/VerifiedBadge';
 import { Code } from '../components/Code';
 import { Input } from '../components/Input';
 import { Image } from '../components/Image';
@@ -712,7 +713,7 @@ function Home() {
                 </Text>
                 <Flex css={{ ai: 'center', gap: '$1' }}>
                   <Kbd>
-                    GRGEGERRE
+                    Cmd
                   </Kbd>
                   <Box css={{ color: '$gray800' }}>
                     <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
@@ -727,6 +728,24 @@ function Home() {
                     Up
                   </Kbd>
                 </Flex>
+              </Flex>
+            </Container>
+          </Section>
+
+          <Flex css={{ jc: 'center' }}>
+            <Separator size="2" />
+          </Flex>
+
+          <Section size={3}>
+            <Container size="2">
+              <Flex css={{ ai: 'baseline' }}>
+                <Text size="3" css={{ fontWeight: '500' }}>
+                  Colm Tuite
+                </Text>
+                <VerifiedBadge css={{ as: 'center', mx: '$1' }} />
+                <Text size="3" css={{ color: '$gray900' }}>
+                  @colmtuite
+                </Text>
               </Flex>
             </Container>
           </Section>
