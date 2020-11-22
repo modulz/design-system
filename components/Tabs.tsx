7c2d6d81954dfe2cb00bc0ee152f4afc68b8f04b
Colm Tuite <colmtuite@gmail.com>
2020-11-22 08:22:31 +0000

    Update Tabs styling.

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -33,64 +33,41 @@ export function Tabs(props: TabsProps) {
 }
 
 const StyledTab = styled(TabsPrimitive.Tab, {
-  position: 'relative',
   flexShrink: 0,
-  height: '$6',
+  height: '$5',
   display: 'inline-flex',
   lineHeight: 1,
   fontSize: '$2',
-  px: '$3',
+  px: '$2',
   userSelect: 'none',
   outline: 'none',
   alignItems: 'center',
   justifyContent: 'center',
   color: '$gray900',
+  border: '1px solid transparent',
+  borderTopLeftRadius: '$2',
+  borderTopRightRadius: '$2',
+  zIndex: '10',
 
   '&:hover': {
     color: '$hiContrast',
   },
 
-  '::after': {
-    position: 'absolute',
-    content: '""',
-    left: 0,
-    bottom: 0,
-    width: '100%',
-    height: 2,
-    backgroundColor: '$blue800',
-    opacity: 0,
-    transform: 'scaleX(.75)',
-    transition: 'transform 175ms cubic-bezier(0.22, 1, 0.36, 1)',
-  },
-
   '&[data-state="active"]': {
     color: '$hiContrast',
-    '::after': {
-      opacity: 1,
-      transform: 'scaleX(1)',
-    }
+    borderColor: '$gray500',
+    borderBottomColor: 'white',
   },
 
   '&[data-orientation="vertical"]': {
     justifyContent: 'flex-start',
-
-    '::after': {
-      transform: 'scaleX(1)',
-      transform: 'scaleY(.75)',
-      transitionDuration: '350ms',
-    },
+    borderTopRightRadius: 0,
+    borderBottomLeftRadius: '$2',
+    borderBottomColor: 'transparent',
 
     '&[data-state="active"]': {
-      '::after': {
-        bottom: 'auto',
-        left: 'auto',
-        right: 0,
-        width: 2,
-        height: '100%',
-        '::after': {
-          transform: 'scaleY(1)',
-        }
-      }
+      borderBottomColor: '$gray500',
+      borderRightColor: 'white',
     },
   },
 });
@@ -102,12 +79,11 @@ const Tab = React.forwardRef<HTMLDivElement, TabsTabProps>((props, forwardedRef)
 const StyledTabsList = styled(TabsPrimitive.List, {
   flexShrink: 0,
   display: 'flex',
-  borderBottom: '1px solid $gray500',
+  boxShadow: 'inset 0 -1px 0 $gray500',
 
   '&[data-orientation="vertical"]': {
     flexDirection: 'column',
-    borderBottom: 'none',
-    borderRight: '1px solid $gray500',
+    boxShadow: 'inset -1px 0 0 $gray500',
   }
 });
 
