7558089d9457caaf64cec850fa34725b22f66dfb
Pedro Duarte <contact@peduarte.com>
2020-11-23 14:05:16 +0100

    Update ProgressBar.tsx

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -61,7 +61,7 @@ const StyledProgressBar = styled(ProgressBarPrimitive, {
         backgroundColor: '$blue800',
       },
       gradient: {
-        background:
+        backgroundImage:
           'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)',
       },
     },
