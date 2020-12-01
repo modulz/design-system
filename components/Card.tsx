c9b087aac12cbae9de2767f628fa4148c4f911f6
Colm Tuite <colmtuite@Colms-MacBook-Air.local>
2020-12-01 22:25:58 +0000

    More progress on Card.

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,23 +4,26 @@ export type CardProps = StitchesProps<typeof Card>;
 export type CardVariants = StitchesVariants<typeof Card>;
 
 export const Card = styled('div', {
+  backgroundColor: '$panel',
   display: 'block',
   textDecoration: 'none',
   color: 'inherit',
   flexShrink: 0,
-  border: '1px solid $gray600',
+  // border: '1px solid $gray600',
   borderRadius: '$3',
   outline: 'none',
+  boxShadow: '0 0 0 1px rgba(0,0,0,.15)',
 
   variants: {
     interactive: {
       true: {
         ':hover': {
-          borderColor: '$gray700',
+          // borderColor: '$gray700',
+          boxShadow: 'inset 0 0 0 1px rgba(0,0,0,.2)',
         },
         ':focus': {
-          borderColor: '$blue700',
-          boxShadow: '0 0 0 1px $blue700'
+          // borderColor: '$blue700',
+          boxShadow: 'inset 0 0 0 1px $blue700, 0 0 0 1px $blue700, 0 1px 2px rgba(0,0,0,.2)',
         },
       }
     }
