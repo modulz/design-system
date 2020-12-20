1bf3e479743cd1b473347748e8e3cc6ab7246b89
hadihallak <hallak.aa@gmail.com>
2020-12-20 18:28:17 +0400

    t

--- components/Flex.tsx
+++ components/Flex.tsx
@@ -4,4 +4,12 @@ import { flexGapSupported } from './flexGapSupported';
 
 export type FlexProps = StitchesProps<typeof _Flex>;
 
+export const _Flex = styled('div', {
+  // Reset
+  boxSizing: 'border-box',
+
+  // Custom
+  display: 'flex',
+});
+
 export const Flex = ImprovedFlex;
