c811ae2b3e79fe643c2287a48725bfb3cdc60406
Vlad Moroz <hi@vladmoroz.com>
2020-12-21 19:44:13 +0200

    Update simple toggle

--- components/SimpleToggle.tsx
+++ components/SimpleToggle.tsx
@@ -5,30 +5,58 @@ export type SimpleToggleProps = ToggleButtonProps & StitchesProps<typeof SimpleT
 export type SimpleToggleVariants = StitchesVariants<typeof SimpleToggle>;
 
 export const SimpleToggle = styled(ToggleButton, {
-  display: 'inline-flex',
+  // Reset
   alignItems: 'center',
+  appearance: 'none',
+  borderWidth: '0',
+  boxSizing: 'border-box',
+  display: 'inline-flex',
+  flexShrink: 0,
+  fontFamily: 'inherit',
+  fontSize: '14px',
   justifyContent: 'center',
+  lineHeight: '1',
+  outline: 'none',
+  padding: '0',
+  textDecoration: 'none',
+  userSelect: 'none',
+  WebkitTapHighlightColor: 'transparent',
   color: '$hiContrast',
+  '::before': {
+    boxSizing: 'border-box',
+  },
+  '::after': {
+    boxSizing: 'border-box',
+  },
+
   height: '$5',
   width: '$5',
   transition: 'all 40ms linear',
-  backgroundColor: '$gray200',
-
-  '&:hover': {
+  backgroundColor: 'transparent',
+  ':hover': {
+    backgroundColor: '$gray200',
+  },
+  ':active': {
     backgroundColor: '$gray300',
+  },
+  ':focus': {
+    boxShadow: 'inset 0 0 0 1px $gray700, 0 0 0 1px $gray700',
+    zIndex: 1,
   },
 
   '&[data-state="on"]': {
-    backgroundColor: '$gray600',
+    backgroundColor: '$gray400',
     '&:hover': {
-      backgroundColor: '$gray700',
+      backgroundColor: '$gray400',
+    },
+    '&:active': {
+      backgroundColor: '$gray600',
     },
   },
 
   variants: {
     shape: {
       circle: {
-        backgroundColor: 'transparent',
         borderRadius: '$round',
       },
       square: {
